c094a8d7d18f1d81e669bdacbbe6aca5e7257007
Paulo Henrique Silva <ph.silva@gmail.com>
2006-10-12 10:42:14 +0000

    * criada uma lista separada para gerenciar os modules a serem instalados

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,12 +2,21 @@
 
 from distutils.core import setup
 
+uts_modules = ['uts',
+               'uts.core',
+               'uts.controllers',
+               'uts.drivers',
+               'uts.interfaces',
+               'uts.instruments',
+               'uts.util',
+               'uts.util.etree']
+
 setup(name='uts',
       version='0.1',
       description='UTS python wrappers',
       author='P. Henrique Silva',
       author_email='heneique@astro.ufsc.br',
-      packages=['uts', 'uts.core', 'uts.controllers', 'uts.drivers', 'uts.interfaces', 'uts.instruments', 'uts.util', 'uts.util.etree'],
+      packages=uts_modules,
       package_data={'uts.core': ['log.config']},
-      scripts=['bin/uts']
-      )
+      scripts=['bin/uts'])
+
