f880945304170f34d3a358b3e245333c4759054b
Paulo Henrique Silva <ph.silva@gmail.com>
2006-12-06 21:34:11 +0000

    * atualizado para modelo ILifeCycle

--- src/python/uts/controllers/sample.py
+++ src/python/uts/controllers/sample.py
@@ -3,7 +3,7 @@ import time
 
 from uts.core.lifecycle import BasicLifeCycle
 
-class Sample(Controller):
+class Sample(BasicLifeCycle):
 
     def __init__(self, manager):
         BasicLifeCycle.__init__(self, manager)
@@ -14,17 +14,18 @@ class Sample(Controller):
     def shutdown(self):
         pass
 
-    def main(self):
+    def control(self):
+        pass
 
-        def slewCompleteCb(a, b, c):
-            print a
+    def slewCompleteCb(self, a, b, c):
+        print a
 
         # instrument
         tel = self.manager.getInstrument("/Telescope/lx200gps")
 
         if tel:
             print tel.getDec()
-            tel.slewComplete += slewCompleteCb
+            tel.slewComplete += self.slewCompleteCb
         else:
             logging.error("There are no instrument /Telescope/lx200gps avaiable")
 
