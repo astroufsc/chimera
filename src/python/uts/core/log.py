412c6162b0d48a73bf4a524d9a74e81f5bb3e17f
Paulo Henrique Silva <ph.silva@gmail.com>
2006-12-06 21:33:22 +0000

    * cmdlinedriver.py: movido para sandbox * log.py: futura implementacao dos logs

---
+++ src/python/uts/core/log.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+#! -*- coding: iso-8859-1 -*-
+
+import logging
+
+logging.basicConfig(level=logging.WARNING,
+                    format='%(asctime)s %(levelname)s %(module)s:%(lineno)d %(message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S (%j)')
+
