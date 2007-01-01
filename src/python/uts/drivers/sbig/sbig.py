1eda38a445cecd5a4bafe7180deae570684eebd8
Paulo Henrique Silva <ph.silva@gmail.com>
2007-01-01 18:49:25 +0000

    * modificado para utilizar a nova classe Config

--- src/python/uts/drivers/sbig/sbig.py
+++ src/python/uts/drivers/sbig/sbig.py
@@ -4,6 +4,7 @@ import logging
 import pwd
 import threading
 import string
+import random
 
 # ask to use numpy
 os.environ["NUMERIX"] = "numpy"
@@ -26,30 +27,33 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 
 	def init(self, config):
 
-		if config.ccd == "imaging":
+		self.config += config
+
+		if self.config.ccd == "imaging":
 			self.ccd = SBIGDrv.imaging
-		elif config.ccd == "tracking":
+		else:
 			self.ccd = SBIGDrv.tracking
 
+		if self.config.device == "usb":
+			self.dev = SBIGDrv.usb
+		else:
+			self.dev = SBIG.lpt1
+			
+		self.open(self.dev)
+
 	def shutdown(self):
-		pass
+		self.close ()
 
 	def control(self):
 		pass
 
         def open(self, device):
 
-		if device == "usb":
-			dev = SBIGDrv.usb
-		else:
-			dev = SBIG.lpt1
-			
-
 		if not self.drv.openDriver():
 			logging.error("Error opening driver: %d %s." % self.drv.getError())
 			return False
 
-		if not self.drv.openDevice(dev):
+		if not self.drv.openDevice(device):
 			logging.error("Error opening device: %d %s." % self.drv.getError())
 			return False
 
@@ -73,15 +77,19 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 
 	def expose(self, config):
 
+		self.config += config
+
 		self.term.clear()
 
-		if self._expose(config):
-			self._readout(config)
+		if self._expose():
+			self._readout()
 			return True
 		
 		return False
 
-	def abortExposure(self, readout = True):
+	def abortExposure(self, config):
+
+		self.config += config
 
 		if not self.exposing():
 			logging.debug("There are no exposition in course... abort cancelled.")
@@ -93,44 +101,44 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 
 		return True
 
-	def _expose(self, config):
+	def _expose(self):
 
-		if config.shutter == "open":
+		if self.config.shutter == "open":
 			shutter = SBIGDrv.openShutter
-		elif config.shutter == "close":
+		elif self.config.shutter == "close":
 			shutter = SBIGDrv.closeShutter
-		elif config.shutter == "leave":
+		elif self.config.shutter == "leave":
 			shutter = SBIGDrv.leaveShutter
 		else:
 			logging.error("Incorrect shutter option (%s). Leaving shutter intact" % config.shutter)
-			shutter = leaveShutter
+			shutter = SBIGDrv.leaveShutter
 
-		if not self.drv.startExposure(self.ccd, int(config.exptime), shutter):
+		if not self.drv.startExposure(self.ccd, self.config.exp_time, shutter):
 			err = self.drv.getError()
 			logging.error("Error starting exposure: %d %s" %  (err))
 			return False
 
 		# save time exposure started
-		config.start_time = time.time()
+		self.config.start_time = time.time()
 
 		while self.drv.exposing(self.ccd):
 					
 			# check if user asked to abort
 			if self.term.isSet():
 				# ok, abort and check if user asked to do a readout anyway
-				self._endExposure(config)
+				self._endExposure()
 				
-				if int(config.readout_aborted):
-					self._readout(config)
+				if self.config.readout_aborted:
+					self._readout()
 
 				return False
 
 		# end exposure and returns
-		self._endExposure(config)
+		self._endExposure()
 
 		return True
 
-	def _endExposure(self, config):
+	def _endExposure(self):
 
 		if not self.drv.endExposure(self.ccd):
 			err = self.drv.getError()
@@ -142,11 +150,11 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 
 		return True
 
-	def _readout(self, config):
+	def _readout(self):
 
 		# start readout
 		# FIXME: bitpix selection
-                img = numpy.zeros(self.drv.readoutModes[self.ccd][int(config.readout_mode)].getSize(), numpy.int16)
+                img = numpy.zeros(self.drv.readoutModes[self.ccd][self.config.readout_mode].getSize(), numpy.int16)
 
 		if not self.drv.startReadout(self.ccd, 0):
 			err = self.drv.getError()
@@ -154,33 +162,30 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 			return False
 
                 i = 0
-                for line in range(self.drv.readoutModes[self.ccd][int(config.readout_mode)].height):
+                for line in range(self.drv.readoutModes[self.ccd][self.config.readout_mode].height):
                         img[i] = self.drv.readoutLine(self.ccd, 0)
                         i = i + 1
 
 			# check if user asked to abort
 			if self.term.isSet():
-				self._endReadout(config)
-				self._saveFITS(config, img)
+				self._endReadout()
+				self._saveFITS(img)
 				
 				return True
 
 		# end readout and save
-		self._endReadout(config)
-		self._saveFITS(config, img)
+		self._endReadout()
+		self._saveFITS(img)
 
 		return True
 
-	def _saveFITS(self, config, img):
+	def _saveFITS(self, img):
                                         
 		# check if config.directory exists
 		# also check write permissions. If user don't have permission, try to write on /tmp
 		# and log this so user can try to copy this later
 
-		#config.dateformat = '%d%m%y'
-		#config.fileformat = '$num-$observer-$date-%objname'
-
-		dest = config.directory
+		dest = self.config.directory
 
 		dest = os.path.expanduser(dest)
 		dest = os.path.expandvars(dest)
@@ -189,7 +194,7 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 		# existence of the directory
 		if not os.path.exists(dest):
 			# directory doesn't exist, check config to know what to do
-			if not config.save_on_temp == "true":
+			if not self.config.save_on_temp:
 				logging.warning("The direcotry specified (%s) doesn't exist "
 						"and save_on_temp was not active, the current"
 						"exposure will be lost." % (dest))
@@ -199,7 +204,7 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 		# permission
 		if not os.access(dest, os.W_OK):
 			# user doesn't have permission to write on dest, check config to know what to do
-			if not config.save_on_temp == "true":
+			if not self.config.save_on_temp:
 
 				uid = os.getuid()
 				user = pwd.getpwuid(uid)[0]
@@ -208,22 +213,26 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 						"exposure will be lost." % (user, uid, dest))
 
 				return False
+
 		# create filename
 		# FIXME: UTC or not UTC?
-		date = time.strftime(config.date_format, time.gmtime(config.start_time))
-		subs_dict = {'num': int(config.seq_num),
-			     'observer': config.observer,
+		date = time.strftime(self.config.date_format, time.gmtime(self.config.start_time))
+		subs_dict = {'num': self.config.seq_num,
+			     'observer': self.config.observer,
 			     'date': date,
-			     'objname': config.obj_name}
+			     'objname': self.config.obj_name}
 		
-		filename = string.Template(config.file_format).safe_substitute(subs_dict)
-		finalname = os.path.join(dest, "%s%s%s" % (filename, os.path.extsep,config.file_extension))
+		filename = string.Template(self.config.file_format).safe_substitute(subs_dict)
+		finalname = os.path.join(dest, "%s%s%s" % (filename, os.path.extsep, self.config.file_extension))
 
-# 		# check if the finalname doesn't exist
-# 		if os.path.exists(finalname):
-# 			# what to do
+		# check if the finalname doesn't exist
+ 		if os.path.exists(finalname):
+			tmp = finalname
+			finalname = os.path.join(dest, "%s-%d%s%s" % (filename, int (random.random()*1000),
+								       os.path.extsep, self.config.file_extension))
+			
+ 			logging.debug ("Image %s already exists. Saving to %s instead." %  (tmp, finalname))
 
-		# ok, finally save the file
 
 		try:
 			hdu  = pyfits.PrimaryHDU(img)
@@ -232,7 +241,7 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 			# any other header should be added later by the controller
 			fits_date_format = "%Y-%m-%dT%H:%M:%S"
 			date = time.strftime(fits_date_format, time.gmtime())
-			dateobs = time.strftime(fits_date_format, time.gmtime(config.start_time))
+			dateobs = time.strftime(fits_date_format, time.gmtime(self.config.start_time))
 
 			hdu.header.update("DATE", date, "date of file creation")
 			hdu.header.update("DATE-OBS", dateobs, "date of the observation")
@@ -241,14 +250,17 @@ class SBIG(BasicLifeCycle, ICameraDriver):
 			fits.writeto(finalname)
 
 		except IOError, e:
-			logging.error("An error ocurred trying to save on %s. The current image will be lost" %
-				      (finalname, e.message))
+			logging.error("An error ocurred trying to save on %s."
+				      "The current image will be lost."
+				      "Exception follow" %  finalname)
+
+			logging.exception(e)
 			
 			return False
 
 		return finalname
 
-	def _endReadout(self, config):
+	def _endReadout(self):
 			
 		if not self.drv.endReadout(self.ccd):
 			err = self.drv.getError()
