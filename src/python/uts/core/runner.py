7c0fad8d1a727c70e3adab81af8ae11a19f32606
Paulo Henrique Silva <ph.silva@gmail.com>
2007-01-12 16:34:45 +0000

    * nova classe responsavel pela inicializacao das thread e gerenciamento dos sinais   (separada do Site para permitir usar a API do sistema de forma mais facil)

---
+++ src/python/uts/core/runner.py
@@ -0,0 +1,62 @@
+import os
+import signal
+import threading
+
+# ============
+# FIXME Ugly hacks to python threading works with signal
+# FIXME see http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/496735
+# FIXME see http://greenteapress.com/semaphores/
+
+# FIXME run shutdown on exit
+# FIXME log
+
+class Runner (object):
+
+    def __init__ (self, obj):
+        self.obj = obj
+
+        self.mainPID = os.getpid()
+
+    
+    def main(self):
+
+        shutdown = threading.Event ()
+
+        # from here we will have 2 process. Child process will return from splitAndWatch,
+        # while the main process will watch for signals and will kill the child
+        # process.
+    
+        self.splitAndWatch()
+
+        pid = os.getpid ()
+
+        # run obj.init on child process
+        if pid != self.mainPID:
+            self.obj.init ()
+
+
+    def splitAndWatch(self):
+
+        child = os.fork()
+
+        if child == 0:
+            return
+
+        self.childPID = child
+
+        signal.signal(signal.SIGTERM, self.sighandler)
+        signal.signal(signal.SIGINT, self.sighandler)
+
+        try:
+            os.wait()
+            self.kill()
+            
+        except OSError:
+            pass
+
+    def kill(self):
+        os.kill (self.childPID, signal.SIGKILL)
+
+    def sighandler(self, sig, frame):
+        self.kill()
+
