4889cb488454c8ff3d055fe114d664036209a807
Paulo Henrique Silva <ph.silva@gmail.com>
2007-01-26 16:55:22 +0000

    * preparando o release.

--- update-fixme-todo.sh
+++
@@ -1,2 +0,0 @@
-find . -type f -name *.c -or -name *.py -or -name *.h -or -name *.cpp | xargs grep -n FIXME >> FIXME
-find . -type f -name *.c -or -name *.py -or -name *.h -or -name *.cpp | xargs grep -n TODO >> TODO
