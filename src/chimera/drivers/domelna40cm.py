f9cd9e1f0e4666e7632447eaf3bf51d219b3899c
Paulo Henrique Silva <ph.silva@gmail.com>
2008-03-12 13:14:41 +0000

    2008-03-12  P. Henrique Silva  <henrique@astro.ufsc.br>
    	* src/chimera/drivers/domelna40cm.py:
    	Converted to new MetaObject framework.
    	Converted to use exceptions.
    	Converted to use full event-driven system.
    
    	Remove old documentation.
    	Remove unneeded debug of protocol.
    
    	IMPORTANT: Add phase difference between dome and scope (180
    	deg). So, dome receive a coord and convert to its own reference
    	frame and do vice-versa when returning on getAz. Azimuth is ALWAYS
    	in telescope reference frame.

--- src/chimera/drivers/domelna40cm.py
+++ src/chimera/drivers/domelna40cm.py
@@ -20,195 +20,141 @@
 
 import time
 import serial
-import signal
 import threading
 import logging
 import math
 import sys
 
-from chimera.core.lifecycle import BasicLifeCycle
-from chimera.interfaces.dome import IDomeDriver
+from chimera.core.chimeraobject import ChimeraObject
+from chimera.interfaces.dome    import InvalidDomePositionException
 
-class DomeLNA40cm (BasicLifeCycle, IDomeDriver):
+from chimera.core.exceptions import ChimeraException
+from chimera.core.lock import lock
+from chimera.interfaces.domedriver import IDomeDriver
 
-    def __init__(self, manager):
+from chimera.util.coord import Coord
 
-        BasicLifeCycle.__init__ (self, manager)
+
+class DomeLNA40cm (ChimeraObject, IDomeDriver):
+
+    def __init__(self):
+        ChimeraObject.__init__ (self)
 
         self.tty = None
         self.abort = threading.Event ()
-        self.slewing = False
 
-        self._errorNo = 0
-        self._errorString = ""
+        self._slewing  = False
+        self._slitOpen = False # FIXME: how to get this information?
 
-        self.timeslice = 0.1
+        self._az_shift = 180
 
-    # -- IBasicLifeCycle implementation --
-    
-    def init (self, config):
-
-        self.config += config
-
-        if not self.open ():
-            return False
-
+    def __start__ (self):
+        self.open()
         return True
 
-    def shutdown (self):
-
-        if not self.close ():
-            return False
-
+    def __stop__ (self):
+        self.close()
         return True
 
-    #def control (self):
-    #    logging.info (self.getAz())
-
-    # -- IDomeDriver implementation
-        
     def _checkDome (self):
-
-        #tmp = self.tty.timeout
-        #self.tty.timeout = 10
-
-        # PH: isto deve ser um ciclo de perguntas tipo: estas acordado?
-        # temos que comecar perguntando para a cupula qual seu azimute
-        # se vem resposta sinal que estah acordada, certo?
-        name = self.getAz()
-        # serial tem timeout embutido
-        # se der merda retorna False
-
-        # _readline, _write, _read tem que buscar do meade.py
-        # o instrumento nao deve chamar coisas que comecao com _
-        # soh deveriam ser usados aqui dentro
-        # _ eh equivalente ao protected no C++
-
-        if not name and self.getError():
-            self.setError (-1, "No response from dome")
-            return False
-
+        # just for test, ask azimuth
+        self.getAz()
         return True
 
     def _checkQuirk (self):
         # clear uC command buffer sending and \r, this makes the uC happy!
         self._write("")
 
-        inv = self._readline ()
-        if inv != "INVALIDO":
-            self.setError (-1, "Quirk error!!!")
-            return False
+        ret = self._readline ()
+        if ret != "INVALIDO":
+            print "quirk", '"%"' % ret
+            raise ChimeraException ("Quirk error!!!")
 
         return True
 
-    def open(self):
+    def open (self):
 
-        self.device = self.config.device
+        self.tty = serial.Serial(self["device"], baudrate=9600,
+                                 bytesize=serial.EIGHTBITS,
+                                 parity=serial.PARITY_NONE,
+                                 stopbits=serial.STOPBITS_ONE,
+                                 timeout=None, xonxoff=0, rtscts=0)
 
-        self.tty = serial.Serial(self.config.device,
-                                 baudrate=9600,         #baudrate
-                                 bytesize=serial.EIGHTBITS,    #number of databits
-                                 parity=serial.PARITY_NONE,    #enable parity checking
-                                 stopbits=serial.STOPBITS_ONE, #number of stopbits
-                                 timeout=None,             #set a timeout value, None for waiting forever
-                                 xonxoff=0,             #enable software flow control
-                                 rtscts=0,              #enable RTS/CTS flow control
-                          )
 
-        try:
-            self.tty.open()
-            self.tty.flushInput ()
-            self.tty.flushOutput ()
+        self.tty.open()
+        self.tty.flushInput ()
+        self.tty.flushOutput ()
 
-            # FIXME: better ping test
-            if not self._checkDome():
-                logging.warning ("Dome is not responding at%s." %  self.config.device)
-                return False
+        self._checkDome()
 
-            return True
-
-        except serial.SerialException, e:
-            self.setError (-1, str(e))
-            logging.debug ("Error while opening %s. Exception follows..." % self.config.device)
-            logging.exception ()
-            return False
-            
-        except IOError,e:
-            self.setError(e)
-            logging.debug ("Error while opening %s. Exception follows..." % self.config.device)
-            logging.exception ()
-            return False
-
-    # disconnect - ***
     def close(self):
         if self.tty.isOpen():
             self.tty.close()
-            return True
-        else:
-            self.setError(-1, "Device not open")
-            return False
 
+    @lock
     def slewToAz(self, az):
 
-        self._checkQuirk ()
-            
-        dome_az = int (math.ceil (az / self.config.az_res))
+        self._checkQuirk()
+
+        # correct dome/telescope phase difference
+        dome_az = az.D + self._az_shift
+        dome_az = dome_az % 360
+        
+        dome_az = int (math.ceil (dome_az / self["az_resolution"]))
 
         pstn = "CUPULA=%03d" % dome_az
 
-        self.tty.setTimeout (self.config.slew_timeout)
+        self.tty.setTimeout (self["slew_timeout"])
 
-        logging.debug (" slew sent %s" % pstn)
-        sys.stdout.flush ()
         self._write(pstn)
-        
+
         ack = self._readline ()
-        logging.debug (" slew ack: %s" % ack)
-        sys.stdout.flush ()
-        
+
         if ack == "INVALIDO":
-            self.setError (-1, "Error trying to slew the dome to azimuth '%s' (dome azimuth '%s')." % (az, dome_az))
-            return False
+            raise IOError("Error trying to slew the dome to"
+                          "azimuth '%s' (dome azimuth '%s')." % (az, dome_az))
 
         # ok, we are slewing now
-        self.slewing = True
+        self._slewing = True
+        self.slewBegin(az)
 
         fin = self._readline ()
-        logging.debug (" slew fin: %s" % fin)
-        sys.stdout.flush ()
 
         if fin == "ALARME":
-            logging.warning (-2, "Error while slewing dome. Some barcodes couldn't be read correctly. Restarting the dome and trying again.")
             # FIXME: restart the dome and try again
-            return False
+            raise IOError("Error while slewing dome. Some barcodes"
+                          " couldn't be read correctly."
+                          " Restarting the dome and trying again.")
 
         if fin.startswith ("CUPULA="):
-            self.slewing = False
+            self._slewing = False
             time.sleep (0.3) # FIXME: how much sleep we need?
-            return True
+            self.slewComplete(self.getAz())
         else:
-            self.setError (-1, "Unknow error while slewing. Received '%s' from dome." % fin)
-            self.slewing = False
-            return False
+            self._slewing = False
+            raise IOError("Unknow error while slewing. Received '%s' from dome." % fin)
 
     def isSlewing (self):
-        return self.slewing
+        return self._slewing
 
-#     def abortSlew(self):
-#         if not self.isSlewing ():
-#             return False
-        
-#         self._checkQuirk ()
+    @lock
+    def abortSlew(self):
 
-#         self._write("PARAR")
+        if not self.isSlewing(): return
 
-#         self.tty.setTimeout (self.config.abort_timeout)
-#         ack = self._readline ()
+        self._checkQuirk ()
 
-#         if ack != "PARADO":
-#             self.setError (-1, "Error while trying
+        self._write("PARAR")
 
+        self.tty.setTimeout (self["abort_timeout"])
+        ack = self._readline ()
 
+        if ack != "PARADO":
+            raise IOError("Error while trying to stop the dome.")
+
+        self.abortComplete(self.getAz())
+
+    @lock
     def getAz(self):
 
         self._checkQuirk ()
@@ -217,115 +163,99 @@ class DomeLNA40cm (BasicLifeCycle, IDomeDriver):
 
         cmd = "POSICAO?"
 
-        logging.debug (" getAz sent %s" % cmd)
-        sys.stdout.flush ()
-        
         self._write(cmd)
-        
+
         ack = self._readline ()
-        logging.debug (" getAz ack %s" % ack)
-        sys.stdout.flush ()        
 
         # check timeout
         if not ack:
-            self.setError (-1, "Couldn't get azimuth after %d seconds." % 10)
-            return False
+            raise IOError("Couldn't get azimuth after %d seconds." % 10)
 
         # uC is going crazy
         if ack == "INVALIDO":
-            self.setError (-1, "Error getting dome azimuth (ack=INVALIDO).")
-            return False
+            raise IOError("Error getting dome azimuth (ack=INVALIDO).")
 
         # get ack return
         if ack.startswith("CUPULA="):
             ack = ack[ack.find("=")+1:]
-            
+
         if ack == "ERRO":
-            logging.error ("Dome is in invalid state. Restarting and trying again.")
             # FIXME: restart and try again
-            return False
+            raise ChimeraException ("Dome is in invalid state. Hard restart needed.")
 
-        self.azimuth = int (math.ceil( int(ack) * self.config.az_res) )
+        # correct dome/telescope phase difference
+        az = int(math.ceil(int(ack)*self["az_resolution"]))
+        az -= self._az_shift
+        az = az % 360
 
-        return self.azimuth
-    
-    def slitOpen (self):
+        return Coord.fromDMS(az)
+
+    @lock
+    def openSlit (self):
 
         self._checkQuirk ()
 
         cmd = "ABRIR"
-        
-        logging.debug (" slitOpen sent %s" % cmd)
-        sys.stdout.flush ()
 
         self._write(cmd)
 
         ack = self._readline()
-        logging.debug (" slitOpen ack %s" % ack)
-        sys.stdout.flush ()        
-        
-        if ack != "ABRINDO":
-            self.setError (-1, "Error trying to open the slit.")
-            return False
 
-        self.tty.setTimeout (self.config.open_timeout)
+        if ack != "ABRINDO":
+            raise IOError("Error trying to open the slit.")
+
+        self.tty.setTimeout (self["open_timeout"])
 
         fin = self._readline ()
-        logging.debug (" slitOpen fin %s" % fin)
-        sys.stdout.flush ()        
 
         # check timeout
         if not fin:
-            self.setError (-1, "Dome is still opening after %d seconds" % self.config.open_timeout)
-            return False
+            raise IOError("Dome is still opening after %d seconds" % self["open_timeout"])
 
-        if fin.startswith ("ABERTO"):
-            return True
-        else:
-            self.setError (-1, "Error opening slit.")
-            return False
+        if not fin.startswith ("ABERTO"):
+            raise IOError("Error opening slit.")
 
-    def slitClose (self):
+        self._slitOpen = True
+        self.slitOpened(self.getAz())
+
+    @lock
+    def closeSlit (self):
         self._checkQuirk ()
 
         cmd = "FECHAR"
 
-        logging.debug (" slitClose sent %s" % cmd)
-        sys.stdout.flush ()
-
         self._write(cmd)
 
         ack = self._readline()
-        logging.debug (" slitClose ack %s" % ack)
-        sys.stdout.flush ()        
-        
-        if ack != "FECHANDO":
-            self.setError (-1, "Error trying to close the slit.")
-            return False
 
-        self.tty.setTimeout (self.config.close_timeout)
+        if ack != "FECHANDO":
+            raise IOError("Error trying to close the slit.")
+
+        self.tty.setTimeout (self["close_timeout"])
 
         fin = self._readline ()
-        logging.debug (" slitClose fin %s" % fin)
-        sys.stdout.flush ()        
 
         # check timeout
         if not fin:
-            self.setError (-1, "Dome is still closing after %d seconds" % self.config.close_timeout)
-            return False
+            raise IOError("Dome is still closing after %d seconds" % self["close_timeout"])
 
-        if fin.startswith ("FECHADO"):
-            return True
-        else:
-            self.setError (-1, "Error closing slit.")
-            return False
+        if not fin.startswith ("FECHADO"):
+            raise IOError("Error closing slit.")
 
+        self._slitOpen = False
+        self.slitClosed(self.getAz())
+
+    def isSlitOpen (self):
+        return self._slitOpen
+
+
+    #
     # low level
-    
+    #
+
     def _read(self, n = 1):
         if not self.tty.isOpen():
-            self.setError(-1, "Device not open")
-            return ""
+            raise IOError("Device not open")
 
         self.tty.flushInput()
 
@@ -333,11 +263,10 @@ class DomeLNA40cm (BasicLifeCycle, IDomeDriver):
 
     def _readline(self, eol="\n"):
         if not self.tty.isOpen():
-            self.setError(-1, "Device not open")
-            return ""
+            raise IOError("Device not open")
 
         self.tty.flushInput()
-        
+
         ret = self.tty.readline(None, eol)
 
         if ret:
@@ -356,43 +285,8 @@ class DomeLNA40cm (BasicLifeCycle, IDomeDriver):
 
     def _write(self, data, eol="\r"):
         if not self.tty.isOpen():
-            self.setError(-1, "Device not open")
-            return ""
+            raise IOError("Device not open")
 
         self.tty.flushOutput()
-        
+
         return self.tty.write("%s%s" % (data, eol))
-
-    def setError(self, errorNo, errorString):
-        self._errorNo = errorNo
-        self._errorString = errorString
-
-    def getError(self):
-        if self._errorNo:
-            ret = (self._errorNo, self._errorString)
-        else:
-            ret = 0
-
-        self._errorNo = 0
-        self._errorString = ""
-
-        return ret
-
-# PH: precisas disto para escrever o driver.
-# Algumas coisas que estao acima e que julgamos boas, nao existem
-# talvez possamos depois pedir para o Chicao implementa-las
-# No início do ano passado havíamos combinado que a princípio seria
-# utilizada a porta serial do PC como meio físico de comunicação com o uC
-# e que os comandos seriam mensagens ASCII. Pensamos em implantar os
-# seguintes comandos:
-# 1) CUPULA nnn<CR>, respostas do uC: GIRANDO <CR> no início do
-# posicionamento e CUPULA=nnn<CR> ao final do posicionamento. Se durante o
-# giro da cúpula, o uC percebesse que alguma etiqueta não foi lida
-# corretamente, ele enviaria a mensagem FALHA=nnn<CR> e se a posição de
-# destino não fosse alcançada, o uC enviaria a mensagem ERRO=nnn<CR>.
-# 2) PARAR<CR>, resposta do uC: PARADO <CR>
-# 3) POSICAO?<CR>, resposta do uC: CUPULA=nnn<CR>
-# 4) ABRIR<CR> (trapeira), resposta do uC: ABRINDO<CR> e ABERTO<CR>
-# 5) FECHAR<CR>, resposta do uC: FECHANDO<CR> e FECHADO<CR>
-# Nota: nnn = posição da cúpula em unidades de 2 graus, faixa de valores
-# válidos: 0 a 179.
