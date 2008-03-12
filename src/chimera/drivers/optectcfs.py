c99b05f1745df1f62961b5b3588c152c97105945
Paulo Henrique Silva <ph.silva@gmail.com>
2008-03-12 13:16:04 +0000

    2008-03-12  P. Henrique Silva  <henrique@astro.ufsc.br>
    	* src/chimera/drivers/optectcfspy:
    	Converted to new MetaObject framework.
    	Converted to use exceptions.
    	Converted to use full event-driven system.

--- src/chimera/drivers/optectcfs.py
+++ src/chimera/drivers/optectcfs.py
@@ -1,5 +1,5 @@
-#! /usr/bin/python
-# -*- coding: iso8859-1 -*-
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
 
 # chimera - observatory automation system
 # Copyright (C) 2006-2007  P. Henrique Silva <henrique@astro.ufsc.br>
@@ -23,92 +23,76 @@ import sys
 
 import serial
 
-from chimera.core.lifecycle import BasicLifeCycle
-from chimera.interfaces.focuser import IFocuserDriver
+from chimera.core.chimeraobject import ChimeraObject
+
+from chimera.interfaces.focuser       import InvalidFocusPositionException
+from chimera.interfaces.focuserdriver import IFocuserDriver
 
 
-class OptecTCFS (BasicLifeCycle, IFocuserDriver):
+from chimera.util.enum import Enum
 
-    def __init__ (self, manager):
-        BasicLifeCycle.__init__ (self, manager)
+
+__all__ = ['OptecTCFS']
+
+
+Mode      = Enum("Manual", "Free", "Auto_A", "Auto_B")
+Direction = Enum("IN", "OUT")
+
+
+class OptecTCFS (ChimeraObject, IFocuserDriver):
+
+    def __init__ (self):
+        ChimeraObject.__init__ (self)
 
         self.tty = None
 
-        self._errorNo = 0
-        self._errorString = ""
+        self._directions = {Direction.IN : "FI",
+                            Direction.OUT: "FO"}
 
+        self._modes = {Mode.Manual: "M",
+                       Mode.Free  : "F",
+                       Mode.Auto_A: "A",
+                       Mode.Auto_B: "B"}
 
-        self._directions = {"IN" : "FI",
-                            "OUT": "FO"}
+    def __start__ (self):
+        self.open()
+        return True
 
-        self._modes = {"M": "Manual",
-                       "F": "Free",
-                       "A": "Auto-A",
-                       "B": "Auto-B"}
-
-    def init (self, config):
-        self.config += config
-
-        if not self.open ():
-            return False
-
+    def __stop__ (self):
+        self.close()
         return True
 
     def open(self):
 
-        self.tty = serial.Serial(self.config.device,
-                                 baudrate=19200,               #baudrate (MUST be 19200 bits)
-                                 bytesize=serial.EIGHTBITS,    #number of databits
-                                 parity=serial.PARITY_NONE,    #enable parity checking
-                                 stopbits=serial.STOPBITS_ONE, #number of stopbits
-                                 timeout=None,                 #set a timeout value, None for waiting forever
-                                 xonxoff=False,                    #enable software flow control
-                                 rtscts=False,                     #enable RTS/CTS flow control
-                                 )
+        self.tty = serial.Serial(self["device"],
+                                 baudrate=19200, # MUST be 19200 bits
+                                 bytesize=serial.EIGHTBITS,
+                                 parity=serial.PARITY_NONE,
+                                 stopbits=serial.STOPBITS_ONE,
+                                 timeout=None,
+                                 xonxoff=False,
+                                 rtscts=False)
 
-        try:
+        self.tty.timeout = self["open_timeout"]
+        self.tty.open()
+        self.tty.flushInput ()
+        self.tty.flushOutput ()
+        
+        self._setMode (Mode.Manual)
 
-            self.tty.timeout = self.config.open_timeout
-            self.tty.open()
-            self.tty.flushInput ()
-            self.tty.flushOutput ()
-            
-            if not self._setMode ("M"):
-                logging.warning ("Couldn't put focuser on manual mode. Remote control will not work.")
-                logging.warning (self.getError ()[1])
-                return False
-
-            if not self._ping():
-                logging.warning ("OPTEC TCF-S is not responding at%s." %  self.config.device)
-                return False
-
-            return True
-
-        except serial.SerialException, e:
-            self.setError (-1, str(e))
-            logging.debug ("Error while opening %s. Exception follows..." % self.config.device)
-            logging.exception ()
-            return False
-            
-        except IOError,e:
-            self.setError(e)
-            logging.debug ("Error while opening %s. Exception follows..." % self.config.device)
-            logging.exception ()
-            return False
+        return True
 
     def close(self):
+
         if self.tty.isOpen():
             self.tty.close()
             return True
-        else:
-            self.setError(-1, "Device not open")
-            return False
 
     def moveIn (self, n):
-        return self._move ("IN", n)
+        return self._move (Direction.IN, n)
 
     def moveOut (self, n):
-        return self._move ("OUT", n)
+        return self._move (Direction.OUT, n)
 
     def moveTo (self, position):
 
@@ -123,54 +107,50 @@ class OptecTCFS (BasicLifeCycle, IFocuserDriver):
         #  IN          OUT
         
         if delta > 0:
-            return self._move ("OUT", abs(delta))
+            return self._move (Direction.OUT, abs(delta))
         elif delta < 0:
-            return self._move ("IN", abs(delta))
+            return self._move (Direction.IN, abs(delta))
         else:
             return True
 
     def _move (self, direction, steps):
 
-        if steps < self.config.min_position or steps > self.config.max_position:
-            self.setError (-1, "Trying to move outside focuser bounds.")
-            return False
-
-        if direction not in self._directions:
-            self.setError (-1, "Invalid direction.")
-            return False
+        if steps < self["min_position"] or steps > self["max_position"]:
+            raise InvalidFocusPositionException("%d is outside focuser limits." % steps)
+        
+        if direction not in Direction:
+            raise ValueError("Invalid direction '%s'." % direction)
 
         cmd = "%s%04d" % (self._directions[direction], steps)
 
-        logging.debug ("  move sent: %s" % cmd)
+        self.log.debug ("  move sent: %s" % cmd)
         sys.stdout.flush ()
 
-        self.tty.timeout = self.config.move_timeout
+        self.tty.timeout = self["move_timeout"]
         
         self._write (cmd)
 
         ack = self._readline (eol="\r")
 
-        logging.debug ("  move ack: %s " % ack[:-2])
+        self.log.debug ("  move ack: %s " % ack[:-2])
         sys.stdout.flush ()
 
         if not ack:
-            self.setError (-1, "Error moving the focuser (read timeout).")
-            return False
+            raise IOError("Error moving the focuser (read timeout).")
 
         if ack.startswith ("ER=2"):
-            # boundary problem... when moving to an upper ow lower bound, ER=2 is returned and the usual * ack comes later
+            # boundary problem... when moving to an upper ow lower bound,
+            #ER=2 is returned and the usual * ack comes later
             ack = ack = self._readline (eol="\r")
 
-            logging.debug ("  move ack: %s " % ack[:-2])
+            self.log.debug ("  move ack: %s " % ack[:-2])
             sys.stdout.flush ()
             
             if not ack:
-                self.setError (-1, "Error moving the focuser (read timeout).")
-                return False
+                raise IOError("Error moving the focuser (read timeout).")
 
         if not ack[:-2] == "*":        
-            self.setError (-1, "Error moving the focuser (back ack).")
-            return False
+            raise ValueError("Error moving the focuser (bad ack).")
 
         return True
 
@@ -179,62 +159,57 @@ class OptecTCFS (BasicLifeCycle, IFocuserDriver):
 
         cmd = "FPOSRO"
         
-        self.tty.timeout = self.config.open_timeout
+        self.tty.timeout = self["open_timeout"]
 
-        logging.debug ("  getPosition sent: %s" % cmd)
+        self.log.debug ("  getPosition sent: %s" % cmd)
         sys.stdout.flush ()
         
         self._write (cmd)
 
         ack = self._readline (eol="\r")
 
-        logging.debug ("  getPosition ack: %s" % ack)
+        self.log.debug ("  getPosition ack: %s" % ack)
         sys.stdout.flush ()
 
         if not ack:
-            self.setError (-1, "Error getting focuser position (read timeout).")
-            return -1
+            raise IOError("Error getting focuser position (read timeout).")
 
         # parse ack and returns
         try:
             return int (ack[2:-2])
         except ValueError, e:
-            self.setError (-1, "Error getting focuser position. Invalid position returned '%s'" % ack)
-            return -1
+            raise IOError("Error getting focuser position. Invalid position returned '%s'" % ack)
 
     def _setMode (self, mode):
 
-        if mode not in self._modes:
-            self.setError (-1, "Invalid mode.")
-            return False
+        if mode not in Mode:
+            raise ValueError("Invalid mode '%s'." % mode)
 
         cmd = "F%sMODE" % mode
 
         # FIXME: well, bad protocol design: diferent modes have diferent ack messages,
         # we only support M mode right now.
-        if mode != "M":
-            self.setError (-1, "%s mode not supported." % self._modes[mode])
-            return False
+        if mode != Mode.Manual:
+            raise NotImplementedError("%s mode not supported." % mode)
 
-        self.tty.timeout = self.config.open_timeout
+        self.tty.timeout = self["open_timeout"]
 
-        logging.debug ("  setMode sent: %s" % cmd)
+        self.log.debug ("  setMode sent: %s" % cmd)
         sys.stdout.flush ()
        
         self._write (cmd)
 
         ack = self._readline (eol="\r")
 
-        logging.debug ("  setMode ack: %s" % ack)
+        self.log.debug ("  setMode ack: %s" % ack)
         sys.stdout.flush ()
 
         if not ack:
-            self.setError (-1, "Error trying to set focuser mode to '%s' (read timeout)." % self._modes[mode])
-            return False
+            raise IOError("Error trying to set focuser mode"
+                          " to '%s' (read timeout)." % self._modes[mode])
 
         if not ack[:-2] == "!":        
-            self.setError (-1, "Error setting focuser mode to '%s' (bad ack)." % self._modes[mode])
-            return False
+            raise IOError("Error setting focuser mode to '%s' (bad ack)." % mode)
 
         return True
 
@@ -242,18 +217,18 @@ class OptecTCFS (BasicLifeCycle, IFocuserDriver):
         return True
     
     def _read(self, n = 1):
+
         if not self.tty.isOpen():
-            self.setError(-1, "Device not open")
-            return ""
+            raise IOError("Device not open")
 
         self.tty.flushInput()
 
         return self.tty.read(n)
 
     def _readline(self, eol="\r"):
+
         if not self.tty.isOpen():
-            self.setError(-1, "Device not open")
-            return ""
+            raise IOError("Device not open")
 
         self.tty.flushInput()
         
@@ -274,24 +249,9 @@ class OptecTCFS (BasicLifeCycle, IFocuserDriver):
 
     def _write(self, data, eol=""):
         if not self.tty.isOpen():
-            self.setError(-1, "Device not open")
-            return ""
+            raise IOError("Device not open")
 
         self.tty.flushOutput()
         
         return self.tty.write("%s%s" % (data, eol))
 
-    def setError(self, errorNo, errorString):
-        self._errorNo = errorNo
-        self._errorString = errorString
-
-    def getError(self):
-        if self._errorNo:
-            ret = (self._errorNo, self._errorString)
-        else:
-            ret = 0
-
-        self._errorNo = 0
-        self._errorString = ""
-
-        return ret
