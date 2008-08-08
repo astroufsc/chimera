66542b9a93c16e0752c1a5eee5481ec421b6f7f4
Antonio Kanaan <ankanaan@gmail.com>
2008-08-08 15:05:59 +0000

    Chimerified pointverify.  It is now a real chimera controller

--- src/chimera/controllers/pointverify.py
+++ src/chimera/controllers/pointverify.py
@@ -1,29 +1,15 @@
-
 from __future__ import division
 
 from chimera.util.catalogs.landolt import Landolt
 from chimera.core.chimeraobject import ChimeraObject
-from chimera.core.lock import lock
-from chimera.core.exceptions import ChimeraException, printException
+from chimera.core.exceptions import ChimeraException
 
 from chimera.interfaces.camera import Shutter
-from chimera.interfaces.filterwheel import Filter
-
-from chimera.util.enum import Enum
-
-from chimera.util.sextractor import SExtractor
 from chimera.util.position import Position
+from chimera.util.coord import Coord
 from chimera.util.image import Image
 
 from chimera.util.astrometrynet import AstrometryNet
-#from chimera.util.ephemerides import Standards
-
-import numpy as N
-import pylab as P
-
-import tempfile
-import time
-from math import sqrt
 
     
 class PointVerify (ChimeraObject):
@@ -56,6 +42,12 @@ class PointVerify (ChimeraObject):
         self.ntrials = 0
         self.max_trials = 10
 
+    def __start__ (self):
+        self.log.warning("LALALA")
+        self.pointVerify()
+        return True
+        
+
     def getTel(self):
         return self.getManager().getProxy(self["telescope"])
 
@@ -69,20 +61,22 @@ class PointVerify (ChimeraObject):
 
         cam = self.getCam()
         
+        #filename = time.strftime("pointverify-%Y%m%d%H%M%S")
         frame = cam.expose(exp_time=self.exptime,
                            frames=1, shutter=Shutter.OPEN,
-                           filename=filename)
-        return frame[0]
-
+                           filename="pointverify-$DATE")
+        #                   filename=filename)
+ 
         if frame:
             imageserver = self.getManager().getProxy(frame[0])
             img = imageserver.getProxyByURI(frame[0])
-            return img.getPath()
+            return("/media/USB2/astindices/demo/dss/NH1_a10d30.fits")
+            #return img.getPath()
 
         else:
-            raise Exception("Couldn't take an image")
+            raise Exception("Could not take an image")
         
-        # return("/media/USB2/astindices/demo/dss/NH1_a10d30.fits")
+
 
     def pointVerify (self):
         """ Checks telescope pointing
@@ -96,57 +90,88 @@ class PointVerify (ChimeraObject):
            take a new image
            test again
            do this while ntrials < max_trials
+
+        Returns True if centering was succesful
+                False if not
+
+
         """
 
         # take an image and read its coordinates off the header
-        image_name = self._takeImage()
+        try:
+            image_name = self._takeImage()
+        except:
+            self.log.error( "Can't take image" )
+            raise
+            
         image = Image.fromFile(image_name) # Image defined in util
         ra_img_center = image["CRVAL1"]    # expects to see this in image
         dec_img_center= image["CRVAL2"]
+        currentImageCenter = Position.fromRaDec(Coord.fromD(ra_img_center), 
+                                                Coord.fromD(dec_img_center))  
+
 
         # analyze the previous image using
         # AstrometryNet defined in util
-        wcs_name = AstrometryNet.solveField(image.filename) 
         try:
-            wcs = Image.fromFile(wcs_name)
+            wcs_name = AstrometryNet.solveField(image.filename) 
         except:
             print "No WCS solution"
+            raise
+        wcs = Image.fromFile(wcs_name)
         ra_wcs_center = wcs["CRVAL1"]
         dec_wcs_center= wcs["CRVAL2"]
+        currentWCS = Position.fromRaDec(Coord.fromD(ra_wcs_center), 
+                                        Coord.fromD(dec_wcs_center))  
+
+        # save the position of first trial:
+        if (self.ntrials == 0):
+            initialPosition = Position.fromRaDec(Coord.fromD(ra_img_center),Coord.fromD(dec_img_center))
+            initialWCS = Position.fromRaDec(currentWCS.ra,currentWCS.dec)
+
+        # write down the two positions for later use in mount models
+        if ( self.ntrials == 0 ):
+            logstr = "Pointing Info for Mount Model: %s %s %s" %(image["DATE-OBS"], initialPosition, currentWCS)
+            self.log.info(logstr)
         
         delta_ra = ra_img_center - ra_wcs_center
         delta_dec = dec_img_center - dec_wcs_center
 
-        print delta_ra, delta_dec
-        print ra_img_center, ra_wcs_center
-        print dec_img_center, dec_wcs_center
+        self.log.debug("%s %s" %(delta_ra, delta_dec))
+        self.log.debug("%s %s" %(ra_img_center, ra_wcs_center))
+        self.log.debug("%s %s" %(dec_img_center, dec_wcs_center))
         
         # *** need to do real logging here
         logstr = "%s %f %f %f %f %f %f" %(image["DATE-OBS"],ra_img_center,dec_img_center,ra_wcs_center,dec_wcs_center,delta_ra,delta_dec)
-        print logstr
+        self.log.debug(logstr)
+
+        tel = self.getTel()
 
         if ( delta_ra > self.tolra ) or ( delta_dec > self.toldec):
             print "Telescope not there yet."
             print "Trying again"
             self.ntrials += 1
-            # *** do I get a telescope everytime? YES, it won't hurt
-            # in python the unused objects are discarded at the end
-            tel = self.getTel()
-            tel.slewToRaDec((coords.ra + delta_ra, 
-                             coords.dec + delta_dec))
             if (self.ntrials > self.max_trials):
                 self.ntrials = 0
                 raise CantPointScopeException("Scope does not point with a precision of %f (RA) or %f (DEC) after %d trials\n" % self.tolra % self.toldec % self.max_trials)
-                return(False)
-            self.point(tel.getPositionRaDec())
-        # if we got here, we were succesfull
-        self.ntrials = 0
+            tel.moveOffset(delta_ra, delta_dec)
+            self.pointVerify()
+        else:
+            # if we got here, we were succesfull reset trials counter
+            self.ntrials = 0
+            # and save final position
+            # write down the two positions for later use in mount models
+            logstr = "Pointing: final solution %s %s %s" %(image["DATE-OBS"],
+                                                           currentImageCenter,
+                                                           currentWCS)
+            self.log.info(logstr)
+
         return(True)
 
 
     def checkPointing(self):
         """
-        This method chooses a field to verify the telescope pointing.
+        This method *chooses* a field to verify the telescope pointing.
         Then it does the pointing and verifies it.
         If unsuccesfull e-mail the operator for help
         isto em portugues eh chamado calagem
@@ -154,12 +179,10 @@ class PointVerify (ChimeraObject):
         # find where the zenith is
         site =  self.getManager().getProxy("/Site/0")
         lst = site.LST()
-        lst = "14:00:00"
         lat = site["latitude"]
-        lat = "-22:00:00"
         coords = Position.fromRaDec(lst,lat)
 
-        # use the Vizier catalogs to see what Landolt field is close to zenith
+        # use the Vizier catalogs to see what Landolt field is closest to zenith
         fld = Landolt()
         fld.useTarget(coords,radius=45)
         obj = fld.find(limit=1)
