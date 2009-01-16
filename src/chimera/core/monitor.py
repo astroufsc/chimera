cb20f7ba297a4230ddc6dafa0c487ecfbbda8b0b
Paulo Henrique Silva <ph.silva@gmail.com>
2009-01-16 04:35:14 +0000

    monitor: fix mistake (Condition is not a class)

--- src/chimera/core/monitor.py
+++ src/chimera/core/monitor.py
@@ -1,7 +1,7 @@
 
 import threading
 
-class Monitor(threading.Condition):
+class Monitor(object):
     """
     Currently, a simple wrapper around a Condition with a Reentrant
     Lock.  Later, more elaborate Monitor, like C# or Java.
