2db3a642e69ba00ea6dcf6420479690a77bb1188
Nelson Saavedra <saavedra.nelson@gmail.com>
2014-04-13 12:14:31 -0300

    Added avt hw driver and fixed chimeraguider.py (missing import!?)

---
+++ src/chimera/instruments/avt/avtdrv.py
@@ -0,0 +1,53 @@
+# ONLY SCRATCH AT THE MOMENT!!!
+import socket, struct
+import xml.etree.ElementTree as etree
+
+
+class AVTDrv(object):
+    ipaddr = '<broadcast>'
+    port = 3956
+    gvcp_hdr_fmt = '>BBBBBBBB'
+    disc_cmd = struct.pack(gvcp_hdr_fmt, 0x42, 0x01, 0x00, 0x02, 0x00, 0x00, 0x00, 0x01)
+    bye_cmd = struct.pack(gvcp_hdr_fmt, 0x42, 0x01, 0x00, 0x04, 0x00, 0x00, 0x00, 0x01)
+    pkt_resend_cmd = struct.pack(gvcp_hdr_fmt, 0x42, 0x01, 0x00, 0x40, 0x00, 0x00, 0x00, 0x01)
+    reg_read_cmd = struct.pack(gvcp_hdr_fmt, 0x42, 0x01, 0x00, 0x80, 0x00, 0x00, 0x00, 0x01)
+    reg_write_cmd = struct.pack(gvcp_hdr_fmt, 0x42, 0x01, 0x00, 0x82, 0x00, 0x00, 0x00, 0x01)
+    mem_read_cmd = struct.pack(gvcp_hdr_fmt, 0x42, 0x01, 0x00, 0x84, 0x00, 0x00, 0x00, 0x01)
+    mem_write_cmd = struct.pack(gvcp_hdr_fmt, 0x42, 0x01, 0x00, 0x86, 0x00, 0x00, 0x00, 0x01)
+
+
+    def __init__(self):
+        #Singleton!?
+        pass
+
+    def GvcpDiscover(self):
+        """
+        Open socket, enable broadcasting, shout for available gigevision cameras,
+        see who responds.
+        @return: response, address tuple
+        """
+        self.sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sk.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+        self.sk.connect((self.ipaddr, self.port))
+        self.sk.sendall(self.disc_cmd)
+        self.resp, self.addr = self.sk.recvfrom(1024)
+        return
+
+
+top = etree.parse('somexmlfile')
+tree = top.getroot()
+
+
+def stripRoot(t, el):
+    return el.tag.lstrip(t.tag + '}')
+
+
+for c in list(tree):
+    if 'Category' in stripRoot(tree, c) and c.attrib['Name'] == 'ImageFormatControl':
+        print [t.text for t in c.getchildren()]
+
+for c in list(tree):
+    if 'Category' in stripRoot(tree, c):
+        print [t.text for t in c.getchildren()]
+
