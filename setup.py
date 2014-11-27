astroufsc/chimera

commit c81e48386a9386d0d03ccafea424bb0e35f022bb

setup.py and requirements.txt sanitization.
Well, somehow, someday we started using both setup.py and requirements.txt
for dependencies definition. They were not in sync and apparently we
are not supposed to do this way according to some sources below.

This changes puts all dependencies data into setup.py alone and requirements.txt
now uses a special flag to use the dependencies from setup.py directly.

In addition I removed the Makefile and windows specific setup/NSI as those are
not supported and/or needed.

For Windows we'll have a wheel in the future to install it easier assuming you
already have Python/pip.

setup.py
@@ -26,95 +26,75 @@ import sys
 import glob
 import re
 
-# append chimera to sys.path to get current version
-build_dir = os.getcwd()
-src_dir = os.path.join(build_dir, 'src')
+# read version.py file to get version and metadata information
+here = os.path.abspath(os.path.dirname(__file__))
+version_py = os.path.join(here, "src/chimera/core/version.py")
+execfile(version_py)
 
-old_sys_path = sys.path
-sys.path.insert(0, src_dir)
-
-from chimera.core.version import _chimera_version_,\
-    _chimera_description_,  \
-    _chimera_author,    \
-    _chimera_author_email_, \
-    _chimera_license_,    \
-    _chimera_url_,    \
-    _chimera_download_url_, \
-    _chimera_classifiers_,  \
-    _chimera_platform_
-
-# modules
-
+# chimera scripts
 chimera_scripts = ['src/scripts/chimera',
-                   'src/scripts/chimera-cam',
-                   'src/scripts/chimera-admin',
-                   'src/scripts/chimera-filter',
-                   'src/scripts/chimera-tel',
-                   'src/scripts/chimera-dome',
-                   'src/scripts/chimera-focus',
-                   'src/scripts/chimera-pverify',
-                   'src/scripts/chimera-console',
-                   'src/scripts/chimera-sched',
-                   'src/scripts/chimera-ppsched',
-                   'src/scripts/chimera-taosched',
-                   'src/scripts/chimera-gui']
+  'src/scripts/chimera-cam',
+  'src/scripts/chimera-admin',
+  'src/scripts/chimera-filter',
+  'src/scripts/chimera-tel',
+  'src/scripts/chimera-dome',
+  'src/scripts/chimera-focus',
+  'src/scripts/chimera-pverify',
+  'src/scripts/chimera-console',
+  'src/scripts/chimera-sched',
+  'src/scripts/chimera-ppsched',
+  'src/scripts/chimera-taosched',
+]
 
-# setup
-
-linux_deps = linux_cdeps = []
-win32_deps = win32_cdeps = []
-mac_deps = mac_cdeps = []
+# platform specific requirements
+platform_deps = []
 
 if sys.platform == "win32":
-    win32_deps += ["pywin32 == 218"]
-elif sys.platform == "darwin":
-    mac_deps += [
-        "python-sbigudrv == 0.5",
-        "pywcs"
-    ]
+  platform_deps += [
+    "pywin32 == 218"
+  ]
 else:
-    linux_deps += [
-        "python-sbigudrv == 0.5",
-        "pywcs"
-    ]
+  platform_deps += [
+    "python-sbigudrv",
+    "pywcs"
+  ]
 
-modulefiles = os.listdir("src/chimera/gui/modules/")
-guifiles = []
-for file in modulefiles:
-    if re.match('(.+\.png)|(.+\.xml)', file):
-        guifiles.append("src/chimera/gui/modules/" + file)
+# go!
+setup(
+  name = 'chimera-python',
+  version = _chimera_version_,
+  description = _chimera_description_,
+  long_description = open("docs/site/index.rst").read(),
+  url = _chimera_url_,
 
-setup(name='chimera-python',
-      package_dir={"": "src"},
+  author = _chimera_author_,
+  author_email = _chimera_author_email_,
 
-      packages=find_packages("src", exclude=["*.tests"]),
-      scripts=chimera_scripts,
-      data_files=[("chimera/core", ["src/chimera/core/chimera.sample.config"]),
-                  ("chimera/gui", ["src/chimera/gui/chimera.xml"]),
-                  ("chimera/gui/modules", guifiles)],
-      zip_safe=False,
+  license = _chimera_license_,
 
-      # dependencies are installed bottom up, so put important things last
-      install_requires=["CherryPy",
-                        "PyYAML",
-                        "suds",
-                        "SQLAlchemy",
-                        "pyephem",
-                        "python-dateutil",
-                        "RO",
-                        "pyfits",
-                        "pyserial",
-                        "Pyro"] + linux_deps + win32_deps + mac_deps + linux_cdeps + win32_cdeps + mac_cdeps + ["numpy"],
+  package_dir = {"": "src"},
+  packages = find_packages("src", exclude=["*.tests"]),
 
-      tests_require=["nose", "coverage"],
+  data_files = [
+    ("chimera/core", ["src/chimera/core/chimera.sample.config"])
+  ],
 
-      version=_chimera_version_,
-      description=_chimera_description_,
-      long_description=open("docs/site/index.rst").read(),
-      author=_chimera_author,
-      author_email=_chimera_author_email_,
-      license=_chimera_license_,
-      url=_chimera_url_,
-      download_url=_chimera_download_url_,
-      classifiers=_chimera_classifiers_,
-      platforms=_chimera_platform_)
+  scripts = chimera_scripts,
+
+  # installation happens in the specified order
+  install_requires = [
+    "Pyro",
+    "numpy",
+    "PyYAML",
+    "python-dateutil",
+    "pyephem",
+    "pyfits",
+    "pyserial",
+    "RO",
+    "suds",
+    "SQLAlchemy",
+    "CherryPy",
+  ] + platform_deps,
+
+  tests_require = ["nose", "coverage", "wheel"],
+)

.gitignore
@@ -43,6 +43,4 @@ nosetests.xml
 #Documentation auxfiles
 doc.pdf
 
-#scheduler link
-src/chimera/controllers/scheduler
 .idea

EnvVarUpdate.nsh
@@ -1,327 +1,0 @@
-/**
- *  EnvVarUpdate.nsh
- *    : Environmental Variables: append, prepend, and remove entries
- *
- *     WARNING: If you use StrFunc.nsh header then include it before this file
- *              with all required definitions. This is to avoid conflicts
- *
- *  Usage:
- *    ${EnvVarUpdate} "ResultVar" "EnvVarName" "Action" "RegLoc" "PathString"
- *
- *  Credits:
- *  Version 1.0 
- *  * Cal Turney (turnec2)
- *  * Amir Szekely (KiCHiK) and e-circ for developing the forerunners of this
- *    function: AddToPath, un.RemoveFromPath, AddToEnvVar, un.RemoveFromEnvVar,
- *    WriteEnvStr, and un.DeleteEnvStr
- *  * Diego Pedroso (deguix) for StrTok
- *  * Kevin English (kenglish_hi) for StrContains
- *  * Hendri Adriaens (Smile2Me), Diego Pedroso (deguix), and Dan Fuhry  
- *    (dandaman32) for StrReplace
- *
- *  Version 1.1 (compatibility with StrFunc.nsh)
- *  * techtonik
- *
- *  http://nsis.sourceforge.net/Environmental_Variables:_append%2C_prepend%2C_and_remove_entries
- *
- */
-
-
-!ifndef ENVVARUPDATE_FUNCTION
-!define ENVVARUPDATE_FUNCTION
-!verbose push
-!verbose 3
-!include "LogicLib.nsh"
-!include "WinMessages.NSH"
-!include "StrFunc.nsh"
-
-; ---- Fix for conflict if StrFunc.nsh is already includes in main file -----------------------
-!macro _IncludeStrFunction StrFuncName
-  !ifndef ${StrFuncName}_INCLUDED
-    ${${StrFuncName}}
-  !endif
-  !ifndef Un${StrFuncName}_INCLUDED
-    ${Un${StrFuncName}}
-  !endif
-  !define un.${StrFuncName} "${Un${StrFuncName}}"
-!macroend
-
-!insertmacro _IncludeStrFunction StrTok
-!insertmacro _IncludeStrFunction StrStr
-!insertmacro _IncludeStrFunction StrRep
-
-; ---------------------------------- Macro Definitions ----------------------------------------
-!macro _EnvVarUpdateConstructor ResultVar EnvVarName Action Regloc PathString
-  Push "${EnvVarName}"
-  Push "${Action}"
-  Push "${RegLoc}"
-  Push "${PathString}"
-    Call EnvVarUpdate
-  Pop "${ResultVar}"
-!macroend
-!define EnvVarUpdate '!insertmacro "_EnvVarUpdateConstructor"'
- 
-!macro _unEnvVarUpdateConstructor ResultVar EnvVarName Action Regloc PathString
-  Push "${EnvVarName}"
-  Push "${Action}"
-  Push "${RegLoc}"
-  Push "${PathString}"
-    Call un.EnvVarUpdate
-  Pop "${ResultVar}"
-!macroend
-!define un.EnvVarUpdate '!insertmacro "_unEnvVarUpdateConstructor"'
-; ---------------------------------- Macro Definitions end-------------------------------------
- 
-;----------------------------------- EnvVarUpdate start----------------------------------------
-!define hklm_all_users     'HKLM "SYSTEM\CurrentControlSet\Control\Session Manager\Environment"'
-!define hkcu_current_user  'HKCU "Environment"'
- 
-!macro EnvVarUpdate UN
- 
-Function ${UN}EnvVarUpdate
- 
-  Push $0
-  Exch 4
-  Exch $1
-  Exch 3
-  Exch $2
-  Exch 2
-  Exch $3
-  Exch
-  Exch $4
-  Push $5
-  Push $6
-  Push $7
-  Push $8
-  Push $9
-  Push $R0
- 
-  /* After this point:
-  -------------------------
-     $0 = ResultVar     (returned)
-     $1 = EnvVarName    (input)
-     $2 = Action        (input)
-     $3 = RegLoc        (input)
-     $4 = PathString    (input)
-     $5 = Orig EnvVar   (read from registry)
-     $6 = Len of $0     (temp)
-     $7 = tempstr1      (temp)
-     $8 = Entry counter (temp)
-     $9 = tempstr2      (temp)
-     $R0 = tempChar     (temp)  */
- 
-  ; Step 1:  Read contents of EnvVarName from RegLoc
-  ;
-  ; Check for empty EnvVarName
-  ${If} $1 == ""
-    SetErrors
-    DetailPrint "ERROR: EnvVarName is blank"
-    Goto EnvVarUpdate_Restore_Vars
-  ${EndIf}
- 
-  ; Check for valid Action
-  ${If}    $2 != "A"
-  ${AndIf} $2 != "P"
-  ${AndIf} $2 != "R"
-    SetErrors
-    DetailPrint "ERROR: Invalid Action - must be A, P, or R"
-    Goto EnvVarUpdate_Restore_Vars
-  ${EndIf}
- 
-  ${If} $3 == HKLM
-    ReadRegStr $5 ${hklm_all_users} $1     ; Get EnvVarName from all users into $5
-  ${ElseIf} $3 == HKCU
-    ReadRegStr $5 ${hkcu_current_user} $1  ; Read EnvVarName from current user into $5
-  ${Else}
-    SetErrors
-    DetailPrint 'ERROR: Action is [$3] but must be "HKLM" or HKCU"'
-    Goto EnvVarUpdate_Restore_Vars
-  ${EndIf}
- 
-  ; Check for empty PathString
-  ${If} $4 == ""
-    SetErrors
-    DetailPrint "ERROR: PathString is blank"
-    Goto EnvVarUpdate_Restore_Vars
-  ${EndIf}
- 
-  ; Make sure we've got some work to do
-  ${If} $5 == ""
-  ${AndIf} $2 == "R"
-    SetErrors
-    DetailPrint "$1 is empty - Nothing to remove"
-    Goto EnvVarUpdate_Restore_Vars
-  ${EndIf}
- 
-  ; Step 2: Scrub EnvVar
-  ;
-  StrCpy $0 $5                             ; Copy the contents to $0
-  ; Remove spaces around semicolons (NOTE: spaces before the 1st entry or
-  ; after the last one are not removed here but instead in Step 3)
-  ${If} $0 != ""                           ; If EnvVar is not empty ...
-    ${Do}
-      ${${UN}StrStr} $7 $0 " ;"
-      ${If} $7 == ""
-        ${ExitDo}
-      ${EndIf}
-      ${${UN}StrRep} $0  $0 " ;" ";"         ; Remove '<space>;'
-    ${Loop}
-    ${Do}
-      ${${UN}StrStr} $7 $0 "; "
-      ${If} $7 == ""
-        ${ExitDo}
-      ${EndIf}
-      ${${UN}StrRep} $0  $0 "; " ";"         ; Remove ';<space>'
-    ${Loop}
-    ${Do}
-      ${${UN}StrStr} $7 $0 ";;" 
-      ${If} $7 == ""
-        ${ExitDo}
-      ${EndIf}
-      ${${UN}StrRep} $0  $0 ";;" ";"
-    ${Loop}
- 
-    ; Remove a leading or trailing semicolon from EnvVar
-    StrCpy  $7  $0 1 0
-    ${If} $7 == ";"
-      StrCpy $0  $0 "" 1                   ; Change ';<EnvVar>' to '<EnvVar>'
-    ${EndIf}
-    StrLen $6 $0
-    IntOp $6 $6 - 1
-    StrCpy $7  $0 1 $6
-    ${If} $7 == ";"
-     StrCpy $0  $0 $6                      ; Change ';<EnvVar>' to '<EnvVar>'
-    ${EndIf}
-    ; DetailPrint "Scrubbed $1: [$0]"      ; Uncomment to debug
-  ${EndIf}
- 
-  /* Step 3. Remove all instances of the target path/string (even if "A" or "P")
-     $6 = bool flag (1 = found and removed PathString)
-     $7 = a string (e.g. path) delimited by semicolon(s)
-     $8 = entry counter starting at 0
-     $9 = copy of $0
-     $R0 = tempChar      */
- 
-  ${If} $5 != ""                           ; If EnvVar is not empty ...
-    StrCpy $9 $0
-    StrCpy $0 ""
-    StrCpy $8 0
-    StrCpy $6 0
- 
-    ${Do}
-      ${${UN}StrTok} $7 $9 ";" $8 "0"      ; $7 = next entry, $8 = entry counter
- 
-      ${If} $7 == ""                       ; If we've run out of entries,
-        ${ExitDo}                          ;    were done
-      ${EndIf}                             ;
- 
-      ; Remove leading and trailing spaces from this entry (critical step for Action=Remove)
-      ${Do}
-        StrCpy $R0  $7 1
-        ${If} $R0 != " "
-          ${ExitDo}
-        ${EndIf}
-        StrCpy $7   $7 "" 1                ;  Remove leading space
-      ${Loop}
-      ${Do}
-        StrCpy $R0  $7 1 -1
-        ${If} $R0 != " "
-          ${ExitDo}
-        ${EndIf}
-        StrCpy $7   $7 -1                  ;  Remove trailing space
-      ${Loop}
-      ${If} $7 == $4                       ; If string matches, remove it by not appending it
-        StrCpy $6 1                        ; Set 'found' flag
-      ${ElseIf} $7 != $4                   ; If string does NOT match
-      ${AndIf}  $0 == ""                   ;    and the 1st string being added to $0,
-        StrCpy $0 $7                       ;    copy it to $0 without a prepended semicolon
-      ${ElseIf} $7 != $4                   ; If string does NOT match
-      ${AndIf}  $0 != ""                   ;    and this is NOT the 1st string to be added to $0,
-        StrCpy $0 $0;$7                    ;    append path to $0 with a prepended semicolon
-      ${EndIf}                             ;
- 
-      IntOp $8 $8 + 1                      ; Bump counter
-    ${Loop}                                ; Check for duplicates until we run out of paths
-  ${EndIf}
- 
-  ; Step 4:  Perform the requested Action
-  ;
-  ${If} $2 != "R"                          ; If Append or Prepend
-    ${If} $6 == 1                          ; And if we found the target
-      DetailPrint "Target is already present in $1. It will be removed and"
-    ${EndIf}
-    ${If} $0 == ""                         ; If EnvVar is (now) empty
-      StrCpy $0 $4                         ;   just copy PathString to EnvVar
-      ${If} $6 == 0                        ; If found flag is either 0
-      ${OrIf} $6 == ""                     ; or blank (if EnvVarName is empty)
-        DetailPrint "$1 was empty and has been updated with the target"
-      ${EndIf}
-    ${ElseIf} $2 == "A"                    ;  If Append (and EnvVar is not empty),
-      StrCpy $0 $0;$4                      ;     append PathString
-      ${If} $6 == 1
-        DetailPrint "appended to $1"
-      ${Else}
-        DetailPrint "Target was appended to $1"
-      ${EndIf}
-    ${Else}                                ;  If Prepend (and EnvVar is not empty),
-      StrCpy $0 $4;$0                      ;     prepend PathString
-      ${If} $6 == 1
-        DetailPrint "prepended to $1"
-      ${Else}
-        DetailPrint "Target was prepended to $1"
-      ${EndIf}
-    ${EndIf}
-  ${Else}                                  ; If Action = Remove
-    ${If} $6 == 1                          ;   and we found the target
-      DetailPrint "Target was found and removed from $1"
-    ${Else}
-      DetailPrint "Target was NOT found in $1 (nothing to remove)"
-    ${EndIf}
-    ${If} $0 == ""
-      DetailPrint "$1 is now empty"
-    ${EndIf}
-  ${EndIf}
- 
-  ; Step 5:  Update the registry at RegLoc with the updated EnvVar and announce the change
-  ;
-  ClearErrors
-  ${If} $3  == HKLM
-    WriteRegExpandStr ${hklm_all_users} $1 $0     ; Write it in all users section
-  ${ElseIf} $3 == HKCU
-    WriteRegExpandStr ${hkcu_current_user} $1 $0  ; Write it to current user section
-  ${EndIf}
- 
-  IfErrors 0 +4
-    MessageBox MB_OK|MB_ICONEXCLAMATION "Could not write updated $1 to $3"
-    DetailPrint "Could not write updated $1 to $3"
-    Goto EnvVarUpdate_Restore_Vars
- 
-  ; "Export" our change
-  SendMessage ${HWND_BROADCAST} ${WM_WININICHANGE} 0 "STR:Environment" /TIMEOUT=5000
- 
-  EnvVarUpdate_Restore_Vars:
-  ;
-  ; Restore the user's variables and return ResultVar
-  Pop $R0
-  Pop $9
-  Pop $8
-  Pop $7
-  Pop $6
-  Pop $5
-  Pop $4
-  Pop $3
-  Pop $2
-  Pop $1
-  Push $0  ; Push my $0 (ResultVar)
-  Exch
-  Pop $0   ; Restore his $0
- 
-FunctionEnd
- 
-!macroend   ; EnvVarUpdate UN
-!insertmacro EnvVarUpdate ""
-!insertmacro EnvVarUpdate "un."
-;----------------------------------- EnvVarUpdate end----------------------------------------
- 
-!verbose pop
-!endif

Makefile
@@ -1,2 +1,0 @@
-chimera:
-	@cd src && PYTHONPATH=.:$$PYTHONPATH python scripts/chimera

setup-win32.py
@@ -1,87 +1,0 @@
-#! /usr/bin/python
-# -*- coding: utf-8 -*-
-
-# chimera - observatory automation system
-# Copyright (C) 2006-2007  P. Henrique Silva <henrique@astro.ufsc.br>
-
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-from distutils.core import setup
-from setuptools import find_packages
-
-import py2exe
-import os
-import sys
-import glob
-
-
-# append chimera to sys.path to get current version
-build_dir = os.getcwd ()
-src_dir   = os.path.join (build_dir, 'src')
-
-old_sys_path = sys.path
-sys.path.insert (0, src_dir)
-
-from chimera.core.version import  _chimera_version_,		\
-                                  _chimera_description_,	\
-                                  _chimera_author,		\
-                                  _chimera_author_email_,	\
-                                  _chimera_license_,		\
-                                  _chimera_url_,		\
-                                  _chimera_download_url_,	\
-                                  _chimera_classifiers_,	\
-                                  _chimera_platform_		
-
-# modules
-
-chimera_scripts = ['src/scripts/chimera',
-                   'src/scripts/chimera-cam',
-                   'src/scripts/chimera-admin',
-                   'src/scripts/chimera-filter',
-                   'src/scripts/chimera-tel',
-                   'src/scripts/chimera-dome',
-                   'src/scripts/chimera-focus',
-                   'src/scripts/chimera-pverify',
-                   'src/scripts/chimera-sched']
-                   
-
-chimera_gui = ['src/scripts/chimera-gui']
-
-# setup
-
-setup(name='chimera-python',
-      data_files       = [("samples", ["src/chimera/core/chimera.sample.config"])],
-      
-      console          = chimera_scripts,
-      
-      options          = {'py2exe': {'packages': 'encodings, chimera',
-                                     'excludes': 'Tkconstants, Tkinter, FixTk, tcl, tk, email, _ssl, win32ui, gtk, glib, gobject, cairo',
-                                     'optimize': 2,
-                                     'bundle_files': 3,
-                                     'compressed': True
-                                    }
-                         },
-      
-      version          = _chimera_version_,
-      description      = _chimera_description_,
-      long_description = open("docs/site/index.rst").read(),
-      author           = _chimera_author,
-      author_email     = _chimera_author_email_,
-      license          = _chimera_license_,
-      url              = _chimera_url_,
-      download_url     = _chimera_download_url_,
-      classifiers      = _chimera_classifiers_,
-      platforms        = _chimera_platform_
-)

setup.cfg
@@ -16,11 +16,6 @@ with-id = True
 
 tests = src/chimera/core,src/chimera/util
 
-[bdist_rpm]
-release   = 1
-group     = Applications/Engineering
-provides  = chimera-core
-
 [sdist]
 formats = zip,bztar,gztar
 

setup.nsi
@@ -1,93 +1,0 @@
-
-!define CHIMERA "Chimera"
-!define CHIMERA_VERSION "0.2" 
-!define CHIMERA_BRANDINGTEXT "Chimera - Observatory Automation System"
-
-!include "MUI2.nsh"
-!include "EnvVarUpdate.nsh"
-
-;--------------------------------
-;General
-
-Name ${CHIMERA}
-InstallDir "$PROGRAMFILES\${CHIMERA}"
-
-CRCCheck On
-RequestExecutionLevel user
-
-OutFile "chimera.exe"
-
-ShowInstDetails "hide"
-ShowUninstDetails "hide"
-
-SetCompressor "LZMA"
-
-;--------------------------------
-;Interface Settings
-
-!define MUI_ABORTWARNING
-
-;--------------------------------
-;Pages
-
-!insertmacro MUI_PAGE_WELCOME
-!insertmacro MUI_PAGE_LICENSE "COPYING"
-!insertmacro MUI_PAGE_DIRECTORY
-!insertmacro MUI_PAGE_INSTFILES
-!insertmacro MUI_PAGE_FINISH
-
-!insertmacro MUI_UNPAGE_WELCOME
-!insertmacro MUI_UNPAGE_INSTFILES
-!insertmacro MUI_UNPAGE_FINISH
-
-;--------------------------------
-;Languages
- 
-!insertmacro MUI_LANGUAGE "English"
-
-
-;-------------------------------- 
-;Installer Sections     
-Section "install"
-
-    ;Add files
-    SetOutPath "$INSTDIR"
-    File /r dist\*.*  
-    
-    ${EnvVarUpdate} $0 "PATH" "A" "HKCU" $INSTDIR
-
-    ;create start-menu items
-    CreateDirectory "$SMPROGRAMS\${CHIMERA}"
-    CreateShortCut "$SMPROGRAMS\${CHIMERA}\Uninstall.lnk" "$INSTDIR\Uninstall.exe" "" "$INSTDIR\Uninstall.exe" 0
-
-    ;write uninstall information to the registry
-    WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\${CHIMERA}" "DisplayName" "${CHIMERA}"
-    WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\${CHIMERA}" "UninstallString" "$INSTDIR\Uninstall.exe"
-
-    WriteUninstaller "$INSTDIR\Uninstall.exe"
-
-SectionEnd
-
-
-;--------------------------------    
-;Uninstaller Section  
-Section "Uninstall"
-
-    ;Delete Files 
-    RMDir /r "$INSTDIR\*.*"    
-    
-    ${un.EnvVarUpdate} $0 "PATH" "R" "HKCU" $INSTDIR
-    
-    ;Remove the installation directory
-    RMDir "$INSTDIR"
-
-    ;Delete Start Menu Shortcuts
-    Delete "$SMPROGRAMS\${CHIMERA}\Uninstall.lnk"
-    RMDir  "$SMPROGRAMS\${CHIMERA}"
-
-    ;Delete Uninstaller And Unistall Registry Entries
-    DeleteRegKey HKEY_LOCAL_MACHINE "SOFTWARE\${CHIMERA}"
-    DeleteRegKey HKEY_LOCAL_MACHINE "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\${CHIMERA}"  
-
-SectionEnd
-

src/chimera/core/version.py
@@ -23,12 +23,11 @@
 
 _chimera_version_ = "0.2"
 _chimera_name_ = "chimera"
-_chimera_requires_ = ""
 _chimera_provides_ = "chimera-core"
 
 _chimera_description_ = "Chimera - Observatory Automation System"
 _chimera_long_description_ = "Observatory Automation System"
-_chimera_author = "P. Henrique Silva"
+_chimera_author_ = "P. Henrique Silva"
 _chimera_author_email_ = "henrique@astro.ufsc.br"
 _chimera_license_ = "GPLv2"
 _chimera_url_ = "http://chimera.sf.net"

commit 91f5578eaed57e524bdd5b8bc3f355cf86898e99

Fixed requirements broken by c81e48386a9386d0d03ccafea424bb0e35f022bb

setup.py
@@ -33,65 +33,65 @@ execfile(version_py)
 
 # chimera scripts
 chimera_scripts = ['src/scripts/chimera',
-  'src/scripts/chimera-cam',
-  'src/scripts/chimera-admin',
-  'src/scripts/chimera-filter',
-  'src/scripts/chimera-tel',
-  'src/scripts/chimera-dome',
-  'src/scripts/chimera-focus',
-  'src/scripts/chimera-pverify',
-  'src/scripts/chimera-console',
-  'src/scripts/chimera-sched',
-  'src/scripts/chimera-ppsched',
-  'src/scripts/chimera-taosched',
+                   'src/scripts/chimera-cam',
+                   'src/scripts/chimera-admin',
+                   'src/scripts/chimera-filter',
+                   'src/scripts/chimera-tel',
+                   'src/scripts/chimera-dome',
+                   'src/scripts/chimera-focus',
+                   'src/scripts/chimera-pverify',
+                   'src/scripts/chimera-console',
+                   'src/scripts/chimera-sched',
+                   'src/scripts/chimera-ppsched',
+                   'src/scripts/chimera-taosched',
 ]
 
 # platform specific requirements
 platform_deps = []
 
 if sys.platform == "win32":
-  platform_deps += [
-    "pywin32 == 218"
-  ]
+    platform_deps += [
+        "pywin32 == 218"
+    ]
 else:
-  platform_deps += [
-    "python-sbigudrv"
-  ]
+    platform_deps += [
+        "python-sbigudrv"
+    ]
 
 # go!
 setup(
-  name = 'chimera-python',
-  version = _chimera_version_,
-  description = _chimera_description_,
-  long_description = open("docs/site/index.rst").read(),
-  url = _chimera_url_,
+    name='chimera-python',
+    version=_chimera_version_,
+    description=_chimera_description_,
+    long_description=open("docs/site/index.rst").read(),
+    url=_chimera_url_,
 
-  author = _chimera_author_,
-  author_email = _chimera_author_email_,
+    author=_chimera_author_,
+    author_email=_chimera_author_email_,
 
-  license = _chimera_license_,
+    license=_chimera_license_,
 
-  package_dir = {"": "src"},
-  packages = find_packages("src", exclude=["*.tests"]),
+    package_dir={"": "src"},
+    packages=find_packages("src", exclude=["*.tests"]),
 
-  include_package_data = True,
+    include_package_data=True,
 
-  scripts = chimera_scripts,
+    scripts=chimera_scripts,
 
-  # installation happens in the specified order
-  install_requires = [
-    "Pyro",
-    "numpy",
-    "astropy",
-    "PyYAML",
-    "python-dateutil",
-    "pyephem",
-    "pyserial",
-    "RO",
-    "suds",
-    "SQLAlchemy",
-    "CherryPy",
-  ] + platform_deps,
+    # installation happens in the specified order
+    install_requires=[
+                         "Pyro>=3.16",
+                         "numpy>=1.8.0",
+                         "astropy",
+                         "PyYAML",
+                         "python-dateutil",
+                         "pyephem",
+                         "pyserial",
+                         "RO",
+                         "suds",
+                         "SQLAlchemy",
+                         "CherryPy",
+                     ] + platform_deps,
 
-  tests_require = ["nose", "coverage", "wheel"],
+    tests_require=["nose", "coverage", "wheel"],
 )