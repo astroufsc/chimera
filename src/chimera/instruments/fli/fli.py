d651ffb39adffc6c1427f1fb4a51dcbe8a9a3fba
Chimera <chimera@Chimera.(none)>
2015-01-31 12:25:33 -0300

    Working version; completed chimera integration. Contrib: Tiago Ribeiro.

--- src/chimera/instruments/fli/fli.py
+++ src/chimera/instruments/fli/fli.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import logging
+import time
 
 #from FLI.device import USBDevice
 from chimera.instruments.fli.flidrv.camera import USBCamera
@@ -7,8 +8,13 @@ from chimera.instruments.fli.flidrv.camera import USBCamera
 
 from chimera.instruments.camera import CameraBase
 
-from chimera.interfaces.camera import (CCD, CameraFeature)
+from chimera.interfaces.camera import (CCD, CameraFeature,
+                                       ReadoutMode,
+                                       CameraStatus)
 
+from chimera.core.lock import lock
+
+import datetime as dt
 
 log = logging.getLogger(__name__)
 
@@ -42,9 +48,22 @@ class FLI(CameraBase):
         self._supports = {CameraFeature.TEMPERATURE_CONTROL: True,
                           CameraFeature.PROGRAMMABLE_GAIN: False,
                           CameraFeature.PROGRAMMABLE_OVERSCAN: True,
-                          CameraFeature.PROGRAMMABLE_FAN: False,
+                          CameraFeature.PROGRAMMABLE_FAN: True,
                           CameraFeature.PROGRAMMABLE_LEDS: True,
                           CameraFeature.PROGRAMMABLE_BIAS_LEVEL: False}
+
+        # my internal CCD code
+        self._MY_CCD = 1 << 1
+        self._MY_ADC = 1 << 2
+        self._MY_READOUT_MODE = 1 << 3
+
+        self._ccds = {self._MY_CCD: CCD.IMAGING}
+
+        self._adcs = {"12 bits": self._MY_ADC}
+
+        self._binnings = {"1x1": self._MY_READOUT_MODE}
+
+        self._binning_factors = {"1x1": 1}
 
         # Kludge: this is a camera class, let's assume we're talking to a
         # camera!
@@ -65,6 +84,21 @@ class FLI(CameraBase):
         self.width, self.height, self.imgsz = self.thecam.get_image_size()
         self.pixelWidth, self.pixelHeight = self.info['pixel_size']
         self.log.info('Camera: %s', self.info)
+
+
+        readoutMode = ReadoutMode()
+        readoutMode.mode = 0
+        readoutMode.gain = 1.0
+        readoutMode.width = self.width
+        readoutMode.height = self.height
+        readoutMode.pixelWidth = self.pixelWidth
+        readoutMode.pixelHeight = self.pixelHeight
+        self._readoutModes = {self._MY_CCD:
+                             {self._MY_READOUT_MODE: readoutMode}}
+
+
+    def __stop__(self):
+        pass
 
     def getSize(self):
         """
@@ -122,6 +156,7 @@ class FLI(CameraBase):
         """
         pass
 
+    @lock
     def getTemperature(self):
         """
         .. method:: getTemperature()
@@ -167,22 +202,27 @@ class FLI(CameraBase):
             return True
 
     def getCCDs(self):
-        pass
+        '''
+        Provisory. Just so info will work!
+        '''
+        return self._ccds
 
     def getCurrentCCD(self):
-        pass
+        return self._MY_CCD
 
     def getBinnings(self):
-        pass
+        return self._binnings
 
     def getADCs(self):
-        pass
+        # Also provisory! Justo so info will work... 
+        return self._adcs
 
     def getPhysicalSize(self):
-        pass
+        return self.width,self.height
 
     def getOverscanSize(self):
-        pass
+        # Provisory
+        return 0,0
 
     def getReadoutModes(self):
         """
@@ -195,9 +235,9 @@ class FLI(CameraBase):
 
             :return: dict of dicts describing per ccd modes.
         """
-        pass
+        return self._readoutModes
 
-    def expose(self, request=None, **kwargs):
+    def _expose(self, request):
         """
         .. method:: expose(request=None, **kwargs)
 
@@ -211,13 +251,19 @@ class FLI(CameraBase):
         # NOTE: AFAIK, there is no way an ImageRequest kw will be left
         # with no value: if no ImageRequest is passed, any value not
         # covered by kwargs will get a default from chimera-cam...right?
+        ftype='normal'
         if request is not None:
             # debug
             self.log.debug('ImageRequest received')
             exptime = int(request['exptime'])
-            ftype = request['type']
+            self.log.debug(request)
+            if request['type'] == 'bias' or request['type'] == 'dark':
+                ftype = 'dark' #request['type']
             # Is this the correct order?
-            hbin, vbin = request['binning'].split('x')
+            if request['binning']:
+                hbin, vbin = request['binning'].split('x')
+            else:
+                hbin,vbin = 1,1
             # It seems the bitdepth from the API is buggy... We leave it at
             # its 16bit default. Next!
         else:
@@ -234,13 +280,76 @@ class FLI(CameraBase):
             else:
                 ftype = 'dark'
             hbin, vbin = [1, 1]
-        self.thecam.set_flushes(8)
+        
+        
+        self.log.debug('Setting flush of the CCD to 1...')
+        self.thecam.set_flushes(1)
+        self.log.debug('Setting binning of the CCD to %ix%i...'%(hbin,vbin))
         self.thecam.set_image_binning(hbin, vbin)
-        self.thecam.set_exposure(exptime, frametype=ftype)
+        self.log.debug('Setting exposure time to %f and frametype to %s...'%(exptime,ftype))
+        self.thecam.set_exposure(exptime*1000,ftype) # miliseconds
+        #self.log.debug(request)
         # Signal the event
+        #return 0
+
         self.exposeBegin(request)
         # All set up, shoot. This method returns immediately.
+        self.log.debug('Starting exposure...')
         self.thecam.start_exposure()
+
+        status = CameraStatus.OK
+        self.lastFrameStartTime = dt.datetime.utcnow()
+        self.lastFrameTemp = self.getTemperature()
+
+        timestart = time.time()
+        
+        while self.isExposing():
+
+            self.log.debug('Exposing ...')
+
+            # [ABORT POINT]
+            if self.abort.isSet():
+                # [TIAGO] Need to do some cleaning at camera level?
+                status = CameraStatus.ABORTED
+                break
+            elif ( (time.time()-timestart) > 2.0*exptime):
+                self.log.warning('Exposure timed-out')
+                status = CameraStatus.ABORTED
+                break
+            # this sleep is EXTREMELY important: without it, Python would stuck on this
+            # thread and abort will not work.
+            time.sleep(5.0)
+
+
+        self.exposeComplete(request, status)
+        return True
+
+        # end exposure and returns
+    #return self._endExposure(imageRequest, status)
+
+    def _readout(self,imageRequest):
+
+        (mode, binning, top,  left,
+         width, height) = self._getReadoutModeInfo(imageRequest["binning"],
+                                                   imageRequest["window"])
+        # readout
+        #img = N.zeros((height, width), N.int32)
+
+        self.readoutBegin(imageRequest)
+
+        # [TIAGO] : I think this function must be either thread/locked 
+        # or re-writed so that it enables chimera to abort.
+        img = self.thecam.fetch_image()
+
+        proxy = self._saveImage(imageRequest, img,
+                                {"frame_temperature": self.lastFrameTemp,
+                                 "frame_start_time": self.lastFrameStartTime,
+                                 "binning_factor": self._binning_factors[binning]})
+
+        self.log.debug('Sleeping for 5 seconds so camera can recover...')
+        time.sleep(5)
+        self.readoutComplete(proxy, CameraStatus.OK)
+        return proxy
 
     # def abortExposure(self, readout=True):
     #     """
@@ -260,10 +369,7 @@ class FLI(CameraBase):
     #         return self.thecam.fetch_image()
 
     def isExposing(self):
-        if not self.thecam.get_exposure_timeleft():
-            return True
-        else:
-            return False
+        return not (self.thecam.get_exposure_timeleft() == 0)
 
     # Header
     # def getMetadata(self, request):
@@ -272,3 +378,11 @@ class FLI(CameraBase):
     #             ('FILTER', str(self.getFilter()),
     #              'Filter used for this observation')
     #             ]
+
+
+    def supports(self, feature=None):
+        if feature in self._supports:
+            return self._supports[feature]
+        else:
+            return False
+
