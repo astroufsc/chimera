cd9ec81cd937a60c60debf9e2645f7293138d4dd
Salvador Sergi Agati <agati@procele.com.br>
2015-03-29 13:23:41 -0300

    Excluded Dome class from DomeBase extensions in instruments.dome.py. Imports optimized.

--- src/chimera/instruments/dome.py
+++ src/chimera/instruments/dome.py
@@ -24,7 +24,7 @@ import Queue
 import threading
 
 from chimera.core.chimeraobject import ChimeraObject
-from chimera.interfaces.dome import Dome, Mode, DomeSlew, DomeSlit, DomeLights, DomeSync
+from chimera.interfaces.dome import Mode, DomeSlew, DomeSlit, DomeLights, DomeSync
 from chimera.core.lock import lock
 from chimera.core.exceptions import ObjectNotFoundException
 from chimera.core.exceptions import ChimeraException
@@ -34,7 +34,7 @@ from chimera.util.coord import Coord
 __all__ = ['DomeBase']
 
 
-class DomeBase(ChimeraObject, Dome, DomeSlew, DomeSlit, DomeLights, DomeSync):
+class DomeBase(ChimeraObject, DomeSlew, DomeSlit, DomeLights, DomeSync):
     def __init__(self):
         ChimeraObject.__init__(self)
 
