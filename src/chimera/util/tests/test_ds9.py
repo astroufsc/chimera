2cd0e63eeaed296466939cdac0e247f8ea110c2e
William Schoenell <wschoenell@gmail.com>
2015-11-15 12:56:54 -0300

    Reversed last commits from @agati on master of astroufsc.

--- src/chimera/util/tests/test_ds9.py
+++ src/chimera/util/tests/test_ds9.py
@@ -26,7 +26,7 @@ class TestDS9(object):
 
         raise SkipTest()
 
-        filename = os.path.realpath(os.path.join(os.path.dirname(__file__), "weatherstation-script-sem-wcs.fits"))
+        filename = os.path.realpath(os.path.join(os.path.dirname(__file__), "teste-sem-wcs.fits"))
 
         p = subprocess.Popen("ds9 %s" % filename, shell=True)
         time.sleep(2)
