astroufsc/chimera

commit 4e86920fe9e16330b7b8451cf32ed4d44a908072

if imageRequest is None, no compression.

src/chimera/util/image.py
@@ -200,8 +200,6 @@ class Image(DictMixin, RemoteObject):
         # TODO: Implement BITPIX support
         hdu.scale('int16', '', bzero=32768, bscale=1)
 
-        if imageRequest:
-            headers += imageRequest.headers
 
         for h in headers:
             try:
@@ -209,12 +207,14 @@ class Image(DictMixin, RemoteObject):
             except Exception, e:
                 log.warning("Couldn't add %s: %s" % (str(h), str(e)))
 
-        if imageRequest['compress_format'] == 'fits_rice':
-            filename = os.path.splitext(filename)[0] + ".fz"
-            img = fits.CompImageHDU(data=data, header=hdu.header, compression_type='RICE_1')
-            img.writeto(filename, checksum=True)
+        if imageRequest:
+            headers += imageRequest.headers
+            if imageRequest['compress_format'] == 'fits_rice':
+                filename = os.path.splitext(filename)[0] + ".fz"
+                img = fits.CompImageHDU(data=data, header=hdu.header, compression_type='RICE_1')
+                img.writeto(filename, checksum=True)
 
-        else:
+        if not imageRequest or imageRequest['compress_format'] != 'compress_rice':
             hdu.data = data
 
             hduList = fits.HDUList([hdu])

commit 449377fa55d867e495fc6eb98454f92710847eba

Fixes a bug that when compression=rice_1 an IOError exception is thrown due to the tentative of opening an existing file.
	The bug was introduced in 4e86920fe9e16330b7b8451cf32ed4d44a908072 but only noticed after astropy.io.fits started to Except when the file exists.

src/chimera/util/image.py
@@ -214,16 +214,16 @@ class Image(DictMixin, RemoteObject):
                 filename = os.path.splitext(filename)[0] + ".fz"
                 img = fits.CompImageHDU(data=data, header=hdu.header, compression_type='RICE_1')
                 img.writeto(filename, checksum=True)
+                return Image.fromFile(filename)
+            
+        hdu.data = data
 
-        if not imageRequest or imageRequest['compress_format'] != 'compress_rice':
-            hdu.data = data
+        hduList = fits.HDUList([hdu])
+        hduList.writeto(filename)
+        hduList.close()
 
-            hduList = fits.HDUList([hdu])
-            hduList.writeto(filename)
-            hduList.close()
-
-            del hduList
-            del hdu
+        del hduList
+        del hdu
 
         return Image.fromFile(filename)
 