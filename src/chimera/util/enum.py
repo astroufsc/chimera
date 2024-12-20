b37e92ba6e0fdfaa1035893627bd3d56d0178d31
William Schoenell <wschoenell@carnegiescience.edu>
2024-12-20 00:01:25 -0300

    small comment

--- src/chimera/util/enum.py
+++ src/chimera/util/enum.py
@@ -3,4 +3,5 @@ from enum import StrEnum
 
 class Enum(StrEnum):
     def __getitem__(self, name):
+        # still needed for serialization - fixme
         return super().__getitem__(name.split(".")[-1])
