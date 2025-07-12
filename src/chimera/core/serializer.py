611ad6ef13e339af7590fd6fed6697c50e30508e
Paulo Henrique Silva <ph.silva@gmail.com>
2025-07-12 02:42:07 -0300

    chore: fix ruff misconfguration
    ruff was configured to run just two rules instead of adding two more
    rules to the default selection.

--- src/chimera/core/serializer.py
+++ src/chimera/core/serializer.py
@@ -2,3 +2,25 @@ class Serializer:
     def dumps(self, obj): ...
 
     def loads(self, data): ...
+
+    @property
+    def object_mode(self) -> bool: ...
+
+
+class Serializable:
+    def dump(self, serializer: Serializer) -> bytes | None:
+        try:
+            return serializer.dumps(self)
+        except Exception:
+            # TODO: raise this somehow?
+            return None
+
+    @classmethod
+    def load(cls: Type[Self], serializer: Serializer, data: bytes) -> Self | None:
+        try:
+            if serializer.object_mode:
+                return serializer.loads(data)
+            return cls(**serializer.loads(data))
+        except Exception as e:
+            logger.error(f"Failed to load {cls.__name__} from data: {data}", exc_info=e)
+            return None
