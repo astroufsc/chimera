04a62731a38dc8e3c2b0109dd0d12bc4e9af941d
William Schoenell <wschoenell@carnegiescience.edu>
2025-11-03 10:09:27 -0800

    Add Rotator interface

---
+++ tests/test_chimera-rotator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+chimera-rotator -v --version
+chimera-rotator -v --info
+chimera-rotator -v --to=89
+chimera-rotator -v --info
+chimera-rotator -v --by=3
+chimera-rotator -v --info
+chimera-rotator -v --by=-3
+chimera-rotator -v --info
