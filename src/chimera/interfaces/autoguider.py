238a55ad7556276a072fee39c2e27162d85f08de
William Schoenell <wschoenell@carnegiescience.edu>
2026-06-12 12:46:06 -0700

    Complete Autoguider interface and add chimera-guide CLI
    Add start_guiding/stop_guiding/abort/is_guiding/get_status/dither to the
    Autoguider interface, settle and dither configuration, CALIBRATING/
    SETTLING/PAUSED statuses and a dither_complete event.
    
    Add the chimera-guide command line tool to control any Autoguider
    instrument (start/stop/abort/dither/find-star/monitor/info), e.g. the
    PHD2 guider from the chimera-phd2guiding plugin.

--- src/chimera/interfaces/autoguider.py
+++ src/chimera/interfaces/autoguider.py
@@ -11,6 +11,9 @@ from chimera.util.enum import Enum
 class GuiderStatus(Enum):
     OK = "OK"
     GUIDING = "GUIDING"
+    CALIBRATING = "CALIBRATING"
+    SETTLING = "SETTLING"
+    PAUSED = "PAUSED"
     OFF = "OFF"
     ERROR = "ERROR"
     ABORTED = "ABORTED"
@@ -20,17 +23,92 @@ class StarNotFoundException(ChimeraException):
     pass
 
 
+class GuiderException(ChimeraException):
+    pass
+
+
 class Autoguider(Interface):
     __config__ = {
         "site": "/Site/0",  # Telescope Site.
         "telescope": "/Telescope/0",  # Telescope instrument that will be guided by the autoguider.
         "camera": "/Camera/0",  # Guider camera instrument.
         "max_acquire_tries": 3,  # Number of tries to find a guiding star.
-        "max_fit_tries": 3,
-    }  # Number of tries to acquire the guide star offset before being lost.
+        "max_fit_tries": 3,  # Number of tries to acquire the guide star offset before being lost.
+        "settle_pixels": 1.5,  # Maximum guide distance (pixels) to consider guiding settled/stable.
+        "settle_time": 10.0,  # Minimum time (s) the guider must remain below settle_pixels.
+        "settle_timeout": 60.0,  # Maximum time (s) to wait for the guider to settle.
+        "dither_amount": 3.0,  # Default dither amount (pixels).
+        "dither_ra_only": False,  # Dither only in right ascension.
+    }
+
+    def start_guiding(self, recalibrate=False, wait=False):
+        """Start a guiding sequence: acquire a guide star, calibrate if
+        needed and begin sending corrections to the telescope.
+
+        @param recalibrate: Discard the current calibration and recalibrate
+                            before guiding.
+        @type recalibrate: bool
+
+        @param wait: Block until the guider has settled (or failed) instead
+                     of returning right after the sequence is started.
+        @type wait: bool
+
+        Raises:
+            StarNotFoundException: If no guide star could be acquired.
+            GuiderException: If the guider could not be started.
+        """
+        raise NotImplementedError()
+
+    def stop_guiding(self):
+        """Stop the current guiding sequence.
+
+        Raises:
+            GuiderException: If the guider could not be stopped.
+        """
+        raise NotImplementedError()
+
+    def abort(self):
+        """Abort the current guiding sequence as soon as possible."""
+        raise NotImplementedError()
+
+    def is_guiding(self):
+        """Returns True if a guiding sequence is currently active.
+
+        @rtype: bool
+        """
+        raise NotImplementedError()
+
+    def get_status(self):
+        """Returns the current guider status.
+
+        @rtype: GuiderStatus
+        """
+        raise NotImplementedError()
+
+    def dither(self, amount=None, ra_only=None, wait=False):
+        """Offset the guide star lock position by a small random amount
+        and resume guiding at the new position.
+
+        @param amount: Maximum dither offset (pixels). Uses the
+                       'dither_amount' configuration value if None.
+        @type amount: float
+
+        @param ra_only: Dither only in right ascension. Uses the
+                        'dither_ra_only' configuration value if None.
+        @type ra_only: bool
+
+        @param wait: Block until the guider has settled at the new position.
+        @type wait: bool
+
+        Raises:
+            GuiderException: If not guiding or the dither failed.
+        """
+        raise NotImplementedError()
 
     def find_star(self):
         """Attempts to find a guide star in the current field of view.
+
+        Returns the (x, y) position of the selected guide star.
 
         Raises:
             StarNotFoundException: If no star is found.
@@ -57,3 +135,7 @@ class Autoguider(Interface):
     @event
     def star_lost(self):
         """Raised when the guide star is lost."""
+
+    @event
+    def dither_complete(self, offset, status):
+        """Raised when a dither offset finishes settling."""
